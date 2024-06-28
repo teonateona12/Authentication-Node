5943b7de85ba8dbf4f37788ea330f8de8eabfad9
teonateona12 <teona.piranishvili1@gmail.com>
2024-06-28 13:46:58 +0400

    feat: create server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("hai");
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(2000, () => {
+  console.log("hello from server");
+});
