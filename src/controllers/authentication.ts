9207cf5b31954cd5eced230bf5af98f6102f677c
teonateona12 <teona.piranishvili1@gmail.com>
2024-07-01 13:53:34 +0400

    feat: login function

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -2,6 +2,48 @@ import express from "express";
 import { createUser, getUserByEmail } from "../db/users";
 import { authentication, random } from "../helpers";
 
+export const login = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ error: "Missing email, or password" });
+    }
+
+    const user = await getUserByEmail(email).select(
+      "+authentication.salt +authentication.password"
+    );
+
+    if (!user) {
+      return res.sendStatus(400);
+    }
+
+    const expectedHush = authentication(user.authentication.salt, password);
+
+    if (user.authentication.password !== expectedHush) {
+      return res.sendStatus(403);
+    }
+
+    const salt = random();
+    user.authentication.sessionToken = authentication(
+      salt,
+      user._id.toString()
+    );
+
+    await user.save();
+
+    res.cookie("TEONA_AUTH", user.authentication.sessionToken, {
+      domain: "localhost",
+      path: "/",
+    });
+
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
 export const register = async (req: express.Request, res: express.Response) => {
   try {
     const { email, password, username } = req.body;
