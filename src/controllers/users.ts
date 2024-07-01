d546daeeb4b5536fbd4c908a74b18907ea1288a0
teonateona12 <teona.piranishvili1@gmail.com>
2024-07-01 19:35:29 +0400

    feat: delete user

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUsers } from "../db/users";
+import { deleteUserById, getUsers } from "../db/users";
 
 export const getAllUsers = async (
   req: express.Request,
@@ -9,6 +9,22 @@ export const getAllUsers = async (
     const users = await getUsers();
 
     return res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const deleteUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { id } = req.params;
+
+    const deletedUser = await deleteUserById(id);
+
+    return res.json(deleteUser);
   } catch (error) {
     console.log(error);
     return res.sendStatus(400);
